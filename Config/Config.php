509e33f8cb2f87c3f026810227829af22d29fab3
NoeGarcia2022 <noegarcia4789@gmail.com>
2023-08-07 10:36:08 -0600

    Conexion a la base de datos

--- Config/Config.php
+++ Config/Config.php
@@ -1,2 +1,7 @@
 <?php
 const base_url = "http://localhost/SistemaVentaInventario/";
+const host = "localhost";
+const user = "root";
+const pass = "";
+const db = "sistema";
+const charset = "charset=utf8";
---
+++ Controllers/Usuarios.php
@@ -0,0 +1,9 @@
+<?php
+class Usuarios extends Controller
+{
+    public function index()
+    {
+        #no reconoce el metodo getUsuario
+        // print_r($this->model->getUsuario());
+    }
+}

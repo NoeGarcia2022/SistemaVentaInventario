341875ffa38340ae4e09fc2a7141a5330dd144d6
NoeGarcia2022 <noegarcia4789@gmail.com>
2023-08-19 00:34:46 -0600

    Solucionando error que tenia en el Controller, Usuarios.php

--- Controllers/Usuarios.php
+++ Controllers/Usuarios.php
@@ -3,7 +3,10 @@ class Usuarios extends Controller
 {
     public function index()
     {
-        #no reconoce el metodo getUsuario
-        // print_r($this->model->getUsuario());
+        // Crear una instancia del modelo y asignarla a la propiedad 'model'
+        $this->model = new UsuariosModel(); // Reemplaza 'ModeloUsuario' con el nombre de tu modelo
+
+        // Ahora puedes llamar al método 'getUsuario()' en la instancia del modelo
+        print_r($this->model->getUsuario());
     }
 }
--- Views/index.php
+++ Views/index.php
@@ -1,61 +1,66 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <title>Iniciar Session</title>
-        <link href="<?php echo base_url; ?>Assets/css/styles.css" rel="stylesheet" />
-        <script src="<?php echo base_url; ?>Assets/js/all.js" crossorigin="anonymous"></script>
-    </head>
-    <body class="bg-primary">
-        <div id="layoutAuthentication">
-            <div id="layoutAuthentication_content">
-                <main>
-                    <div class="container">
-                        <div class="row justify-content-center">
-                            <div class="col-lg-5">
-                                <div class="card shadow-lg border-0 rounded-lg mt-5">
-                                    <div class="card-header"><h3 class="text-center font-weight-light my-4">Iniciar Session</h3></div>
-                                    <div class="card-body">
-                                        <form>
-                                            <div class="form-floating mb-3">
-                                                <input class="form-control" id="inputEmail" type="text" placeholder="Ingrese Usuario" />
-                                                <label for="inputEmail">Usuario</label>
-                                            </div>
-                                            <div class="form-floating mb-3">
-                                                <input class="form-control" id="inputPassword" type="password" placeholder="Ingrese su Contraseña" />
-                                                <label for="inputPassword">Contraseña</label>
-                                            </div>
-                                            <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
-                                                <a class="btn btn-primary" href="index.html">Login</a>
-                                            </div>
-                                        </form>
-                                    </div>
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Iniciar Session</title>
+    <link href="<?php echo base_url; ?>Assets/css/styles.css" rel="stylesheet" />
+    <script src="<?php echo base_url; ?>Assets/js/all.js" crossorigin="anonymous"></script>
+</head>
+
+<body class="bg-primary">
+    <div id="layoutAuthentication">
+        <div id="layoutAuthentication_content">
+            <main>
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-5">
+                            <div class="card shadow-lg border-0 rounded-lg mt-5">
+                                <div class="card-header">
+                                    <h3 class="text-center font-weight-light my-4">Iniciar Session</h3>
+                                </div>
+                                <div class="card-body">
+                                    <form>
+                                        <div class="form-floating mb-3">
+                                            <input class="form-control" id="inputEmail" type="text" placeholder="Ingrese Usuario" />
+                                            <label for="inputEmail">Usuario</label>
+                                        </div>
+                                        <div class="form-floating mb-3">
+                                            <input class="form-control" id="inputPassword" type="password" placeholder="Ingrese su Contraseña" />
+                                            <label for="inputPassword">Contraseña</label>
+                                        </div>
+                                        <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
+                                            <a class="btn btn-primary" href="index.html">Login</a>
+                                        </div>
+                                    </form>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </main>
-            </div>
-            <div id="layoutAuthentication_footer">
-                <footer class="py-4 bg-light mt-auto">
-                    <div class="container-fluid px-4">
-                        <div class="d-flex align-items-center justify-content-between small">
-                            <div class="text-muted">Copyright &copy; Your Website <?php echo date("Y")?></div>
-                            <div>
-                                <a href="#">Privacy Policy</a>
-                                &middot;
-                                <a href="#">Terms &amp; Conditions</a>
-                            </div>
+                </div>
+            </main>
+        </div>
+        <div id="layoutAuthentication_footer">
+            <footer class="py-4 bg-light mt-auto">
+                <div class="container-fluid px-4">
+                    <div class="d-flex align-items-center justify-content-between small">
+                        <div class="text-muted">Copyright &copy; Your Website <?php echo date("Y") ?></div>
+                        <div>
+                            <a href="#">Privacy Policy</a>
+                            &middot;
+                            <a href="#">Terms &amp; Conditions</a>
                         </div>
                     </div>
-                </footer>
-            </div>
+                </div>
+            </footer>
         </div>
-        <script src="<?php echo base_url; ?>Assets/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-        <script src="<?php echo base_url; ?>Assets/js/scripts.js"></script>
-    </body>
+    </div>
+    <script src="<?php echo base_url; ?>Assets/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    <script src="<?php echo base_url; ?>Assets/js/scripts.js"></script>
+</body>
+
 </html>
